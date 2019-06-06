52911dc3b5a677875aff96523d8d7f1159371b2f
Krankomat <mariofan48@googlemail.com>
2019-06-06 21:59:18 +0200

    Init project

---
+++ Assets/Scripts/CameraMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    public Camera activeCamera;
+    public float speed;
+
+    private Vector3 inputDirection = new Vector2(); 
+    private float inputHorizontal;
+    private float inputVertical; 
+    
+    
+    void Update()
+    {
+        inputHorizontal = Input.GetAxis("Horizontal");
+        inputVertical = Input.GetAxis("Vertical");
+
+        inputDirection.x = inputHorizontal;
+        inputDirection.z = inputVertical;
+        inputDirection.Normalize();
+
+        activeCamera.transform.position += inputDirection * speed * Time.deltaTime; 
+    }
+}
