1676e11728ed94f90b3821f0af695be06c2791d6
Krankomat <mariofan48@googlemail.com>
2019-07-04 23:46:45 +0200

    Add game hud prefab

---
+++ Assets/Scripts/GameHUDDisplayer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameHUDDisplayer : MonoBehaviour
+{
+
+    public Text selectedObjectNameLabel;
+    public Text selectedObjectDescriptionLabel; 
+    
+}
