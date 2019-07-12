e6b83951427110251692c1b3f80e4f6e0b565a2f
Krankomat <mariofan48@googlemail.com>
2019-07-12 22:36:23 +0200

    Remove selection collider container and add individual colliders

--- Assets/Scripts/Selector.cs
+++ Assets/Scripts/Selector.cs
@@ -8,7 +8,6 @@ public class Selector : MonoBehaviour
     public const float SelectionRaycastMaxDistance = 100f; 
 
     public GameObject selectionMarkPrefab;
-    public GameObject selectionColliderContainerPrefab; 
 
     private GameObject selectionMark;
     private GameObject selectionCollider;
@@ -19,7 +18,6 @@ public class Selector : MonoBehaviour
     private void Start()
     {
         AttachSelectionMarkToParent();
-        AttachSelectionColliderToParent(); 
         selectionMark.SetActive(false); 
     }
 
@@ -53,13 +51,6 @@ public class Selector : MonoBehaviour
     private void AttachSelectionMarkToParent()
     {
         selectionMark = Instantiate(selectionMarkPrefab, gameObject.transform, false);
-    }
-
-    
-    // Attaches a selection collider to the parent game object 
-    private void AttachSelectionColliderToParent()
-    {
-        selectionCollider = Instantiate(selectionColliderContainerPrefab, gameObject.transform, false);
     }
 
 }
