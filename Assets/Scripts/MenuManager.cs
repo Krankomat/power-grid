5854f48336fbd5ba03f4a0b91408c5442d621bcd
Krankomat <mariofan48@googlemail.com>
2019-07-13 18:36:11 +0200

    Add building menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public GameObject closeButton;
+
+    private MenuState menuState; 
+
+
+    private enum MenuState
+    {
+        Opened, 
+        Closed
+    }
+
+
+    private void Start()
+    {
+        closeButton.GetComponent<Button>().onClick.AddListener(CloseMenu);
+        CloseMenu(); 
+    }
+
+
+    public void ShowMenu()
+    {
+        gameObject.SetActive(true);
+        menuState = MenuState.Opened;
+    }
+
+
+    public void CloseMenu()
+    {
+        gameObject.SetActive(false);
+        menuState = MenuState.Closed; 
+    }
+
+}
