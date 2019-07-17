bd5617a7669632a6407ba059a28836da38b2d4da
Krankomat <mariofan48@googlemail.com>
2019-07-17 13:00:13 +0200

    Add util scripts EnumAttribute and EnumExtension

---
+++ Assets/Scripts/Util/EnumAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/** 
+ * Base class to extend, if you want to create custom EnumAttributes. 
+ * Look to EnumExtension.cs for more information. 
+ * 
+ * Based on https://www.codeproject.com/Articles/38666/Enum-Pattern.aspx  
+ * Original author: Gong Liu 
+ * Date: 22 Sep 2009 
+ */
+
+[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+public class EnumAttribute : Attribute
+{
+    public EnumAttribute()
+    {
+    }
+}
---
+++ Assets/Scripts/Util/EnumExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+
+/** 
+ * An extension to Enums, so that they can store values within Attributes. 
+ * This way, they are more similar to Enums in Java. 
+ * 
+ * Based on https://www.codeproject.com/Articles/38666/Enum-Pattern.aspx  
+ * Original author: Gong Liu 
+ * Date: 22 Sep 2009 
+ */
+
+public static class EnumExtension  
+{
+
+    // This is an extension method 
+    public static EnumAttribute GetAttr(this Enum value)
+    {
+        Type type = value.GetType();
+        FieldInfo fieldInfo = type.GetField(value.ToString());
+        EnumAttribute[] attributes = (EnumAttribute[]) fieldInfo.GetCustomAttributes(typeof(EnumAttribute), false);
+
+        if (attributes.Length > 0)
+            return attributes[0];
+
+        return null; 
+
+    }
+}
