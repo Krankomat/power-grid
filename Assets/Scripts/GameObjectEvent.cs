89368acf9ce378bc946893b45956d85052b6bb76
Krankomat <mariofan48@googlemail.com>
2019-07-29 19:08:43 +0200

    Refactor code to have custom events inside CustomEvents

--- Assets/Scripts/GameObjectEvent.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.Events;
-
-[System.Serializable]
-public class GameObjectEvent : UnityEvent<GameObject>
-{
-}
---
+++ Assets/Scripts/Util/CustomEvents.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace CustomEvents
+{
+
+    [System.Serializable]
+    public class GameObjectEvent : UnityEvent<GameObject>
+    {
+    }
+
+    [System.Serializable]
+    public class InteractionStateEvent : UnityEvent<InteractionState>
+    {
+    }
+    
+}
