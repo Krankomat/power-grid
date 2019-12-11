bac382ddfa4dbc913e3a7a88bcf9ba634ba1f91d
Lukas Semmler <semmlerlukas@googlemail.com>
2019-12-11 12:32:20 +0100

    Test out collision between triggers

---
+++ Assets/Scripts/FeatureTesting/CollisionTester.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTester : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Huzzah! I have collided with another " + other); 
+    }
+}
