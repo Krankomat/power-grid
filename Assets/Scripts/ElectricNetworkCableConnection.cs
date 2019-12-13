1aaacdb1dc65f1f3b65768b69742b339cec01a49
Lukas Semmler <semmlerlukas@googlemail.com>
2019-12-13 10:48:18 +0100

    Add Connect method overload to CableConnection

--- Assets/Scripts/ElectricNetworkCableConnection.cs
+++ Assets/Scripts/ElectricNetworkCableConnection.cs
@@ -16,8 +16,32 @@ public class ElectricNetworkCableConnection : MonoBehaviour
         cableA = Instantiate(cablePrefab, gameObject.transform);
         cableB = Instantiate(cablePrefab, gameObject.transform); 
     }
-    
 
+
+    public void Connect()
+    {
+        // Check if edge exists 
+        if (edge == null)
+        {
+            Debug.LogError($"ERROR CONNECTING CABLE: Edge in {this} is null. ");
+            return; 
+        }
+
+        // Check if the nodes in the pair are not null 
+        if (edge.nodes.Item1 == null || edge.nodes.Item2 == null)
+            Debug.LogError($"ERROR CONNECTING CABLE: One or both of the edge nodes are null. " +
+                $"Node 1: {edge.nodes.Item1}; Node 2: {edge.nodes.Item2}. ");
+
+        // Actually Connect 
+        ElectricNetworkConnector connector1 = edge.nodes.Item1.connector; 
+        ElectricNetworkConnector connector2 = edge.nodes.Item2.connector;
+        Connect(connector1, connector2); 
+    }
+
+
+    /*
+     * Should not be called directly; instead, add an edge and simply call Connect() 
+     */ 
     public void Connect(ElectricNetworkConnector startConnector, ElectricNetworkConnector endConnector)
     {
         cableA.GetComponent<CableDrawer>().SetTransforms(startConnector.connectionPointA, endConnector.connectionPointA); 
