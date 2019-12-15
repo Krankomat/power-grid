2e85fec87ae5e2176aec0f6fdff0755d23f25450
Lukas Semmler <semmlerlukas@googlemail.com>
2019-12-15 15:41:04 +0100

    Add type to ElectricNetworkElementPanel

--- Assets/Scripts/UI/ElectricNetworkElementPanel.cs
+++ Assets/Scripts/UI/ElectricNetworkElementPanel.cs
@@ -6,9 +6,16 @@ using UnityEngine.UI;
 public class ElectricNetworkElementPanel : MonoBehaviour
 {
     public Text elementLabel;
+    public Type type = Type.Node; 
 
     public void SetText(string newText)
     {
         elementLabel.text = newText; 
+    }
+
+    public enum Type
+    {
+        Node, 
+        Edge 
     }
 }
