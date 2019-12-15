a520c7566eeb77520efe420e169c068bf91a445a
Lukas Semmler <semmlerlukas@googlemail.com>
2019-12-15 14:21:31 +0100

    Add ElectricNetworkElementPanel

---
+++ Assets/Scripts/UI/ElectricNetworkElementPanel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ElectricNetworkElementPanel : MonoBehaviour
+{
+    public Text elementLabel;
+
+    public void SetText(string newText)
+    {
+        elementLabel.text = newText; 
+    }
+}
--- Assets/Scripts/UI/ElectricNetworkPanel.cs
+++ Assets/Scripts/UI/ElectricNetworkPanel.cs
@@ -8,6 +8,17 @@ public class ElectricNetworkPanel : MonoBehaviour
     public Text title; 
     public GameObject networkNodesContainer;
     public GameObject networkEdgesContainer;
+    public Dictionary<ElectricNetworkNode, ElectricNetworkElementPanel> elementPanelsByNode = new Dictionary<ElectricNetworkNode, ElectricNetworkElementPanel>();
+    public Dictionary<ElectricNetworkEdge, ElectricNetworkElementPanel> elementPanelsByEdge = new Dictionary<ElectricNetworkEdge, ElectricNetworkElementPanel>();
+
+    private void Start()
+    {
+        // Remove example content 
+        foreach (Transform child in networkNodesContainer.transform)
+            Destroy(child.gameObject);
+        foreach (Transform child in networkEdgesContainer.transform)
+            Destroy(child.gameObject);
+    }
 
     public void SetTitle(string newTitle)
     {
