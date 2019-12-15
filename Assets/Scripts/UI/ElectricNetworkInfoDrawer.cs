d7b44b944aeb094d1f86b407b4b9a9be81879bff
Lukas Semmler <semmlerlukas@googlemail.com>
2019-12-15 10:51:12 +0100

    Extract methods from Update in ElectricNetworkInfoDrawer

--- Assets/Scripts/UI/ElectricNetworkInfoDrawer.cs
+++ Assets/Scripts/UI/ElectricNetworkInfoDrawer.cs
@@ -11,35 +11,46 @@ public class ElectricNetworkInfoDrawer : MonoBehaviour
 
     private Dictionary<ElectricNetwork, ElectricNetworkPanel> panelByNetwork = new Dictionary<ElectricNetwork, ElectricNetworkPanel>(); 
 
+
     void Start()
     {
         RemoveExampleContent(); 
     }
 
+
     void Update()
     {
-        // Add new Panels if necessary 
+        CreateNewPanelsIfNecessary();
+        RemoveOutdatedPanels(); 
+    }
+
+
+    private void CreateNewPanelsIfNecessary()
+    {
         foreach (ElectricNetwork network in electricNetworkManager.electricNetworks)
         {
             if (panelByNetwork.ContainsKey(network))
                 continue;
 
             GameObject networkPanelGameObject = Instantiate(networkPanelPrefab);
-            networkPanelGameObject.transform.SetParent(networkPanelContainer.transform); 
+            networkPanelGameObject.transform.SetParent(networkPanelContainer.transform);
             ElectricNetworkPanel networkPanel = networkPanelGameObject.GetComponent<ElectricNetworkPanel>();
-            networkPanel.SetTitle("Network " + network.id); 
-            panelByNetwork.Add(network, networkPanel); 
+            networkPanel.SetTitle("Network " + network.id);
+            panelByNetwork.Add(network, networkPanel);
         }
+    }
 
-        // Remove outdated panels (outdated = network of networkPanel no longer exists) 
-        List<ElectricNetwork> possiblyOutdatedNetworks = new List<ElectricNetwork>(panelByNetwork.Keys); 
+
+    private void RemoveOutdatedPanels()
+    {
+        List<ElectricNetwork> possiblyOutdatedNetworks = new List<ElectricNetwork>(panelByNetwork.Keys);
         foreach (ElectricNetwork possiblyOutdatedNetwork in possiblyOutdatedNetworks)
         {
             if (electricNetworkManager.electricNetworks.Contains(possiblyOutdatedNetwork))
                 continue;
 
             panelByNetwork[possiblyOutdatedNetwork].transform.SetParent(null);
-            panelByNetwork.Remove(possiblyOutdatedNetwork); 
+            panelByNetwork.Remove(possiblyOutdatedNetwork);
         }
     }
 
@@ -47,6 +58,5 @@ public class ElectricNetworkInfoDrawer : MonoBehaviour
     {
         networkPanelContainer.transform.DetachChildren(); 
     }
-    
     
 }
